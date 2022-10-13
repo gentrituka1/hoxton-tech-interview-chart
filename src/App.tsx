a756ff6b2a6499582712cddd1961116dbb83f7b7
Gentrit Uka <gentrit-uka@live.com>
2022-10-13 14:42:52 +0200

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 
+import { Bar, BarChart, XAxis, YAxis } from 'recharts'
 import './App.css'
 const studentData = [
   {
@@ -34,8 +35,22 @@ const studentData = [
 // Using the Recharted library, create a graph as similar as you can, to the one in the #Classroom
 
 function App() {
+
+  function barLabel({ x, y, width, height, value }: any) {
+    return (
+      <text x={x + width / 2} y={y + height / 2} fill="#000" textAnchor="middle" dominantBaseline="middle">
+        {value}
+      </text>
+    )
+  }
+
   return <>
-    {/* Code your solution here */}
+    <BarChart width={600} height={300} data={studentData}>
+      <XAxis dataKey="name" /> 
+      <YAxis />
+      <Bar dataKey="submissions.beavers" barSize={30} fill="#da9249" label={barLabel} />
+      <Bar dataKey="submissions.stars" barSize={30} fill="#ffd700" label={barLabel} />
+    </BarChart>
   </>
 
 }

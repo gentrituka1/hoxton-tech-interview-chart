8eca841a07286b2cd95543b638730cd4dfd36cb1
Gentrit Uka <gentrit-uka@live.com>
2022-10-13 15:17:21 +0200

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -55,6 +55,13 @@ function App() {
     )
   }
 
+  function getRandomColor () {
+    const r = Math.floor(Math.random() * 255)
+    const g = Math.floor(Math.random() * 255)
+    const b = Math.floor(Math.random() * 255)
+    return `rgb(${r}, ${g}, ${b})`
+  }
+
   return <>
     <form onSubmit={(event) => {
       event.preventDefault()

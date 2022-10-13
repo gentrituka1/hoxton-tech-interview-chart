438fd8855d1e21fbd43a9f215a0e69d8e079fb6f
Gentrit Uka <gentrit-uka@live.com>
2022-10-13 14:59:38 +0200

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 
+import { useState } from 'react'
 import { Bar, BarChart, XAxis, YAxis } from 'recharts'
 import './App.css'
+
+type Student = {
+  name: string
+  submissions: {
+    beavers: number
+    stars: number
+  }
+}
+
 const studentData = [
   {
     name: 'Stephen',
@@ -35,6 +45,7 @@ const studentData = [
 // Using the Recharted library, create a graph as similar as you can, to the one in the #Classroom
 
 function App() {
+  const [students, setStudents] = useState<Student[]>(studentData)
 
   function barLabel({ x, y, width, height, value }: any) {
     return (
@@ -45,14 +56,73 @@ function App() {
   }
 
   return <>
-    <BarChart width={600} height={300} data={studentData}>
+    <form onSubmit={(event) => {
+      event.preventDefault()
+
+      const newStudent = {
+        name: event.target.name.value,
+        submissions: {
+          beavers: Number(event.target.beavers.value),
+          stars: Number(event.target.stars.value),
+        }
+      }
+
+      setStudents([...students, newStudent])
+    }}>
+      <h2>Add a Student to Chart</h2>
+      <label htmlFor="name">Name</label>
+      <input type="text" name="name" id="name" />
+      <label htmlFor="beavers">Beavers</label>
+      <input type="number" name="beavers" id="beavers" />
+      <label htmlFor="stars">Stars</label>
+      <input type="number" name="stars" id="stars" />
+      <button type="submit">Add Student</button>
+    </form>
+    <BarChart width={600} height={300} data={students}>
       <XAxis dataKey="name" /> 
       <YAxis />
       <Bar dataKey="submissions.beavers" barSize={30} fill="#da9249" label={barLabel} />
       <Bar dataKey="submissions.stars" barSize={30} fill="#ffd700" label={barLabel} />
     </BarChart>
+    <div>
+      Increase Beavers for:
+      {students.map((student) => {
+        return <button onClick={() => {
+          setStudents(students.map((s) => {
+              if(s.name === student.name) {
+                return {
+                  ...s,
+                  submissions: {
+                    ...s.submissions,
+                    beavers: s.submissions.beavers + 1,
+                  }
+                }
+              }
+              return s
+            }))
+        }}>{student.name}</button>
+      })}
+    </div>
+    <div>
+      Increase Stars for:
+      {students.map((student) => {
+        return <button onClick={() => {
+          setStudents(students.map((s) => {
+              if(s.name === student.name) {
+                return {
+                  ...s,
+                  submissions: {
+                    ...s.submissions,
+                    stars: s.submissions.stars + 1,
+                  }
+                }
+              }
+              return s
+            }))
+        }}>{student.name}</button>
+      })}
+    </div>
   </>
-
 }
 
 export default App
